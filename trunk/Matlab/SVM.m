234ecca7fd5ec13c8cbff3bdf7e2793806a28333
bjergekim@gmail.com <bjergekim@gmail.com@1443e88c-8dc1-bbdc-6191-638f9e59fe1e>
2012-10-17 18:22:44 +0000

    Added SVM 2D and 3D

--- trunk/Matlab/SVM.m
+++ trunk/Matlab/SVM.m
@@ -1,20 +1,32 @@
 clear;
 close all;
 load fisheriris;
+
+%kernel = 'rbf'; % Gaussian Radial Basis Funciton kernel
+%kernel = 'quadratic'; % Quadratic kernel
+%kernel = 'polynomial'; % Polynomial kernel with order of 3
+kernel = 'mlp'; % Multilayer Perceptron kernel with diefault scale and bias
+%kernel = 'linear'; % Linear
+
 data = [meas(:,1), meas(:,2)];
 groups = ismember(species,'setosa');
+%groups = ismember(species,'versicolor');
+%groups = ismember(species,'viriginica');
 [train, test] = crossvalind('holdOut',groups);
 cp = classperf(groups);
 figure
-svmStruct = svmtrain(data(train,:),groups(train),'showplot',true);
+svmStruct = svmtrain(data(train,:),groups(train), 'Kernel_Function', kernel, 'showplot',true);
+
 title(sprintf('Kernel Function: %s',...
                func2str(svmStruct.KernelFunction)), 'interpreter','none');
+pause(3);
 classes = svmclassify(svmStruct,data(test,:),'showplot',true);
 classperf(cp,classes,test);
-cp.CorrectRate
-figure
-svmStruct = svmtrain(data(train,:),groups(train),...
-                     'showplot',true,'boxconstraint',1e6);          
-classes = svmclassify(svmStruct,data(test,:),'showplot',true);   
-classperf(cp,classes,test);
-cp.CorrectRate
+error = 1 - cp.CorrectRate
+
+%figure
+%svmStruct = svmtrain(data(train,:),groups(train),...
+%                     'showplot',true,'boxconstraint',1e6);          
+%classes = svmclassify(svmStruct,data(test,:),'showplot',true);   
+%classperf(cp,classes,test);
+%cp.CorrectRate
---
+++ trunk/Matlab/SVM3D.m
@@ -0,0 +1,54 @@
+function [Ctrain, Ctest] = SVM3D(Ynew, Ytnew, Wnew, Wtnew)
+
+kernel = 'rbf'; % Gaussian Radial Basis Funciton kernel
+%kernel = 'quadratic'; % Quadratic kernel
+%kernel = 'polynomial'; % Polynomial kernel with order of 3
+%kernel = 'mlp'; % Multilayer Perceptron kernel with diefault scale and bias
+%kernel = 'linear'; % Linear
+
+sz = size(Ytnew,1);
+data = [Ynew(1:sz,:); Wnew(1:sz,:)]; % Train data with 2 classes and 2 dimensions
+
+% plot data
+% xi=min(data(:,1)); xf=max(data(:,1)); 
+% yi=min(data(:,2)); yf=max(data(:,2));
+% figure, scatter(Ynew(:,1), Ynew(:,2), 'r'), hold on
+% scatter(Wnew(:,1), Wnew(:,2), 'g')
+% axis([xi xf yi yf])
+% title('SVM2D training data V1(red), V2(green)');
+
+%groups = logical([ones(size(Ynew,1),1); zeros(size(Wnew,1),1)]);
+groups = logical([ones(sz,1); zeros(sz,1)]);
+
+%[train, test] = crossvalind('holdOut',groups);
+%cp = classperf(groups);
+
+figure
+svmStruct = svmtrain(data, groups, 'Kernel_Function', kernel, 'showplot',false);
+
+%title(sprintf('Kernel Function: %s',...
+%               func2str(svmStruct.KernelFunction)), 'interpreter','none');
+
+%% SVM test
+% NB - data must be the same size as datatest
+datatest = [Ytnew; Wtnew]; % Test data with 2 classes and 2 dimensions
+t = [ones(size(Ytnew,1),1); zeros(size(Wtnew,1),1)];
+%groupstest = logical(t);
+
+classes = svmclassify(svmStruct, datatest,'showplot',false);
+%classperf(cp, classes, groupstest); 
+%error = 1 - cp.CorrectRate
+
+Ctest = confmat(t, classes) % uses PRTools
+err_test = 1-sum(diag(Ctest))/sum(Ctest(:)) % correct classification percentage
+
+% Plot test set
+% xi=min(datatest(:,1)); xf=max(datatest(:,1)); 
+% yi=min(datatest(:,2)); yf=max(datatest(:,2));
+% figure, scatter(Ytnew(:,1), Ytnew(:,2), 'r'), hold on
+% scatter(Wtnew(:,1), Wtnew(:,2), 'g')
+% axis([xi xf yi yf])
+% title('SVM2D test data V1(red), V2(green)');
+
+Ctrain = 0;
+
